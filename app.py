c29af1940ed98ef846cbf57036eac056f3ece2ae
Victor Francis <francisvictor47@gmail.com>
2022-02-17 01:45:38 -0800

    Update changes

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from dash import Dash, html, dcc, Input, Output
+import altair as alt
+from vega_datasets import data
+alt.data_transformers.enable('data_server')
+
+crime_data = pd.read_csv("/Users/victorfrancis/Victor/UBC-MDS-MCF/MDS/Courses/DSCI_532_viz-2_students/crimedata_csv_AllNeighbourhoods_2021.csv")
+crime_data.head()
+
+# Setup app and layout/frontend
+app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+app.layout = html.Div([
+    html.Iframe(
+        id='scatter',
+        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    dcc.Dropdown(
+        id='xcol-widget',
+        value='TYPE',  # REQUIRED to show the plot on the first page load
+        options=[{'label': col, 'value': col} for col in crime_data.columns])])
+
+# Set up callbacks/backend
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xcol-widget', 'value'))
+def plot_altair(xcol):
+    chart = alt.Chart(crime_data).mark_point().encode(
+        x=xcol,
+        y='TYPE',
+        tooltip='YEAR').interactive()
+    return chart.to_html()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
