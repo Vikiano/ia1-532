8e39dd9627657a90ffaa4ae37f3323cceccc686c
Victor Francis <francisvictor47@gmail.com>
2022-02-17 01:57:12 -0800

    Update changes

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ crime_data.head()
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
+
 app.layout = html.Div([
     html.Iframe(
         id='scatter',
